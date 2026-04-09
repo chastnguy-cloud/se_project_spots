c925092cac043454f834915ff0eae2406a8034e7
Charles Nguyen <charlesnn111@gmail.com>
2026-04-08 19:25:56 -0700

    Spots Stage 5 Submission I

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,39 @@
+// Variable for the "Edit Profile" Button [1]
 const editProfileBtn = document.querySelector(".profile__description-button");
+
+// Variable for the whole "Edit Profile" modal [1]
 const profileDescriptionModal = document.querySelector("#edit-profile-modal");
+
+// Variable for close button on the "Edit Profile" modal [1]
 const editProfileModalCloseBtn =
   profileDescriptionModal.querySelector(".modal__close-btn");
 
+// variables to store inputs from form - textContent will be assigned to basically get the values of the input in the profileNameEl and ProfileDescriptionEl [1]
+const profileNameEl = document.querySelector(".profile__description-title");
+const profileDescriptionEl = document.querySelector(
+  ".profile__description-subtitle",
+);
+// variable for profile name input in "Edit Profile" modal form [1]
+const editProfileNameInput = profileDescriptionModal.querySelector(
+  "#profile-name-input",
+);
+// variable for profile description input in "Edit Profile" modal form [1]
+const editProfileDescriptionInput = profileDescriptionModal.querySelector(
+  "#profile-description-input",
+);
+
+// Variables for "New Post" button [2]
 const newPostBtn = document.querySelector(".profile__button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostModalCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const addCardFormELement = newPostModal.querySelector(".modal__form");
+const nameInput = newPostModal.querySelector("#caption-input");
+const linkInput = newPostModal.querySelector("#card-image-input");
 
+// event listeners for "Edit Profile" modal [1]
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   profileDescriptionModal.classList.add("modal_is-opened");
 });
 
@@ -15,6 +41,18 @@ editProfileModalCloseBtn.addEventListener("click", function () {
   profileDescriptionModal.classList.remove("modal_is-opened");
 });
 
+// Edit Profile submission handler [1]
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  profileDescriptionModal.classList.remove("modal_is-opened");
+}
+
+profileDescriptionModal.addEventListener("submit", handleProfileFormSubmit);
+
+// event listeners for "New Post" modal [2]
 newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
 });
@@ -22,3 +60,12 @@ newPostBtn.addEventListener("click", function () {
 newPostModalCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log(nameInput.value);
+  console.log(linkInput.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+addCardFormELement.addEventListener("submit", handleAddCardSubmit);
