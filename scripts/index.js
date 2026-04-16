a156e34cbc6734c8ad3aa552305536aa29d1ab80
Charles Nguyen <charlesnn111@gmail.com>
2026-04-15 23:23:03 -0700

    Spots Stage 6 Submission 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over teh forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+
 // Variable for the "Edit Profile" Button [1]
 const editProfileBtn = document.querySelector(".profile__description-button");
 
@@ -82,3 +109,7 @@ newPostModalCloseBtn.addEventListener("click", function () {
 
 // event listener that will reset the form in the newPostModal after submit is pressed [2]
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+initialCards.forEach(function (card) {
+  console.log(card.name);
+});
